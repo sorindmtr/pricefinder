84bfa026be5143a0ee032c25abd50dc25afd4e1d
Sorin Dumitru <93326014+sorindmtr@users.noreply.github.com>
2023-12-04 18:31:29 +0100

    Controller PathVariable to RequestParam

--- src/main/java/com/gft/pricefinder/application/ProductFinderController.java
+++ src/main/java/com/gft/pricefinder/application/ProductFinderController.java
@@ -3,7 +3,6 @@ package com.gft.pricefinder.application;
 import com.gft.pricefinder.domain.ProductFinderService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +16,8 @@ public class ProductFinderController {
 
     private final ProductFinderService productFinderService;
 
-    @GetMapping(value = "/{id}", produces = "application/json")
-    public ProductResponse getByIdDateTimeBrand(@PathVariable Long id, @RequestParam LocalDateTime dateOfPrice, @RequestParam Long brandId) {
-        return productFinderService.getByIdDateTimeBrand(id, dateOfPrice, brandId);
+    @GetMapping(produces = "application/json")
+    public ProductResponse getByIdDateTimeBrand(@RequestParam Long productId, @RequestParam LocalDateTime dateOfPrice, @RequestParam Long brandId) {
+        return productFinderService.getByIdDateTimeBrand(productId, dateOfPrice, brandId);
     }
 }
